36d929d55f5db7f715cd683bcaaa671158deefb9
william-phan-vn <hoangviet191@gmail.com>
2023-07-01 07:15:02 +0700

    init spring boot (maven) demo app

---
+++ demoapp/src/main/java/com/springboot/maven/demo/demoapp/DemoappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.maven.demo.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoappApplication.class, args);
+	}
+
+}
